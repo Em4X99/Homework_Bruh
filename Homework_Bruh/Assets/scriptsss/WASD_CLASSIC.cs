637e157fb465ee9f1b2dd8901fa4ccd9fa4dc11d
Em4X99 <158608018+Em4X99@users.noreply.github.com>
2024-09-16 16:05:37 -0400

    added player with WASD controller

---
+++ Homework_Bruh/Assets/scriptsss/WASD_CLASSIC.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_CLASSIC : MonoBehaviour
+{
+    //Variables 
+    public float forceamt = 10f;
+    private int i = 8;
+    private Rigidbody2D square;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find the Rigidbody on the object this script is on
+        square = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If statement to move Right on screen by using D
+        if (Input.GetKey(KeyCode.D))
+        {
+            //make square move right
+            square.AddForce(Vector2.right *  forceamt);
+        }
+
+        //If statement to move right on screen by using A 
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            //make square move left
+            square.AddForce(Vector2.left * forceamt);
+        }
+
+        //If statement to move down on screen by usind S 
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            //make square move down
+            square.AddForce(Vector2.down * forceamt);
+        }
+    
+        //If statement to move up on screen by using W
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            
+            //make square move up
+            square.AddForce(Vector2.up * forceamt);
+        }
+    }   
+}
