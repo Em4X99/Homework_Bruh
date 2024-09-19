671f5a071bfc590001cbf756366acdf69bf7c637
Em4X99 <158608018+Em4X99@users.noreply.github.com>
2024-09-19 16:07:56 -0400

    change color hit

--- Homework_Bruh/Assets/scriptsss/GameManager/Game_Manager.cs
+++ Homework_Bruh/Assets/scriptsss/GameManager/Game_Manager.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Game_Manager : MonoBehaviour
 {
     //Varibles
     public TextMeshProUGUI score;
 
+    public int sc_num;
     // Start is called before the first frame update
     void Start()
     {
         //Make the score 0
-        score.text = "score: 0";
+        sc_num = 0; 
+        score.text = "score: " + sc_num;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        score.text = "score: " + sc_num;
     }
 }
--- Homework_Bruh/Assets/scriptsss/WASD/WASD3.cs
+++ Homework_Bruh/Assets/scriptsss/WASD/WASD3.cs
@@ -1,52 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build.Content;
 using UnityEngine;
 
-public class WASD_CLASSIC : MonoBehaviour
+public class WASD3 : MonoBehaviour
 {
     //Variables 
     public float forceamt = 10f;
     private int i = 8;
     private Rigidbody2D square;
     private Color defaultColor = Color.yellow;
-    public Game_Manager game_manager;
+
     // Start is called before the first frame update
     void Start()
     {
         //Find the Rigidbody on the object this script is on
         square = GetComponent<Rigidbody2D>();
-
-        //Find the game manager
-        game_manager = FindObjectOfType<Game_Manager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //If statement to move Right on screen by using D
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             //make square move right
-            square.AddForce(Vector2.right *  forceamt);
+            square.AddForce(Vector2.right * forceamt);
         }
 
         //If statement to move right on screen by using A 
-        if (Input.GetKey(KeyCode.A)) 
+        if (Input.GetKey(KeyCode.LeftArrow)) 
         {
             //make square move left
             square.AddForce(Vector2.left * forceamt);
         }
 
         //If statement to move down on screen by usind S 
-        if (Input.GetKey(KeyCode.S)) 
+        if (Input.GetKey(KeyCode.DownArrow)) 
         {
             //make square move down
             square.AddForce(Vector2.down * forceamt);
         }
     
         //If statement to move up on screen by using W
-        if (Input.GetKey(KeyCode.W)) 
+        if (Input.GetKey(KeyCode.UpArrow)) 
         {
             
             //make square move up
@@ -56,26 +52,23 @@ public class WASD_CLASSIC : MonoBehaviour
 
     }
     //on collisionenter2D happens when two 2D obbjects with colliders hit each other
-    private void OnCollisionEnter2D(Collision2D collision)
+    /*private void OnCollisionEnter2D(Collision2D collision)
     {
         //make the cube chanje color when it hits player 2 cube
-        if (collision.transform.tag == "Player2")
+        if (collision.transform.tag == "Player1")
         {
             GetComponent<SpriteRenderer>().color = Color.magenta;
         }
         //Make the cube change color when it hits player 3 cube 
-        if (collision.transform.tag == "Player3")
+        if (collision.transform.tag == "Player2")
         {
             GetComponent <SpriteRenderer>().color = Color.red;
         }
-
-        //score + 1
-        game_manager.sc_num++;
     }
     //OnCollisionExit2D, happens when two 2D objects separate
     private void OnCollisionExit2D(Collision2D collision)
     {
      //Every time we exit, we want object to change back to original color
      GetComponent<SpriteRenderer>().color = defaultColor;
-    }
+    }*/
 }
